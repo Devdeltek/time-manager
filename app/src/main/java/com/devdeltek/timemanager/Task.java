a16664d540e617d2402e5ff43382f531a6a25589
Devdeltek <39629179+Devdeltek@users.noreply.github.com>
2021-07-18 15:57:56 -0700

    Commit test

---
+++ app/src/main/java/com/devdeltek/timemanager/Task.java
@@ -0,0 +1,23 @@
+package com.devdeltek.timemanager;
+
+import java.util.Date;
+
+public class Task {
+
+    private String mTitle;
+
+    private Date mStartTime;
+    private Date mEndTime;
+
+    private int mRepeat;
+
+    private String mNotes;
+
+    private int[] mDays;
+    private subTask[] mActivities;
+
+    public Task(String title, Date startTime, Date endTime, int repeat, String notees, int[] days){
+
+    }
+
+}
---
+++ app/src/main/java/com/devdeltek/timemanager/subTask.java
@@ -0,0 +1,4 @@
+package com.devdeltek.timemanager;
+
+public class subTask {
+}
